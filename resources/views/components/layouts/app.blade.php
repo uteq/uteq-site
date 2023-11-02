93521cadcc00481b7e4f773a17a901c52ff88b0f
rienstra <rubenrienstra@gmail.com>
2023-11-02 12:57:06 +0100

    favicon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="application-name" content="{{ config('app.name') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="{{ Vite::image('favicon1.png') }}">
 
     <title>{{ config('app.name') }}</title>
 
